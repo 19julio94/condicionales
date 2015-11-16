eaff8e09e118f4b09a87b96e0bc9a52bbe541a18
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-11-16 09:03:30 +0100

    inicio

---
+++ src/condicionales/Condicionales.java
@@ -0,0 +1,22 @@
+
+
+package condicionales;
+
+
+public class Condicionales {
+
+    
+    public static void main(String[] args) {
+      
+        int num1=6;
+        int num2=6;
+        
+        if(num1>num2){
+            System.out.println(num1+"e maior");
+        }
+        else {
+            System.out.println(num1+"e menor");
+        }
+    }
+    
+}
